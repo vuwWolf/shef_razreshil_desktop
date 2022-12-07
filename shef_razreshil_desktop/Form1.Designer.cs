90ed76a1adcb2a422c4c6b4eb028e6435d26cbe2
White_Wolf <63950114+vuwWolf@users.noreply.github.com>
2022-12-07 23:57:32 +0300

    я ебал эту хуйню

--- shef_razreshil_desktop/Form1.Designer.cs
+++ shef_razreshil_desktop/Form1.Designer.cs
@@ -29,19 +29,41 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(shef));
+            this.button1 = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // result
+            // 
+            resources.ApplyResources(this.result, "result");
+            this.result.Name = "result";
             // 
             // shef
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.button1);
             this.Name = "shef";
             this.ShowIcon = false;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button button1;
+        private Label result;
     }
 }
--- shef_razreshil_desktop/Form1.cs
+++ shef_razreshil_desktop/Form1.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography.X509Certificates;
+
 namespace shef_razreshil_desktop
 {
     public partial class shef : Form
@@ -9,7 +11,31 @@ namespace shef_razreshil_desktop
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            
+        }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string shef_res = null;
+            string shef = "шеф";
+            string happy_shef = "смилостивился и разрешил покурить";
+            string evil_shef = "запретил курить";
+            Random rnd = new Random();
+            int x = rnd.Next(0, 10);
+
+            if (x >= 9)
+            {
+                shef_res = shef + " " + happy_shef;
+                result.Text = shef_res;
+            }
+            else 
+            {
+                shef_res = shef + " " + evil_shef;
+                result.Text = shef_res;
+            }
+
+            result.Visible = true;
+            
         }
     }
 }
