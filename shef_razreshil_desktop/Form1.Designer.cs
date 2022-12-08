d99ef6519fe8fa824944e983cd85350e5247b22d
White_Wolf <63950114+vuwWolf@users.noreply.github.com>
2022-12-08 21:02:57 +0300

    код дописан

--- shef_razreshil_desktop/Form1.Designer.cs
+++ shef_razreshil_desktop/Form1.Designer.cs
@@ -31,12 +31,19 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(shef));
             this.button1 = new System.Windows.Forms.Button();
             this.result = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.shefBoxTrue = new System.Windows.Forms.PictureBox();
+            this.shefBoxFalse = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shefBoxTrue)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shefBoxFalse)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
             resources.ApplyResources(this.button1, "button1");
             this.button1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.ForeColor = System.Drawing.Color.Black;
             this.button1.Name = "button1";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -44,19 +51,48 @@
             // result
             // 
             resources.ApplyResources(this.result, "result");
-            this.result.ForeColor = System.Drawing.SystemColors.HighlightText;
+            this.result.ForeColor = System.Drawing.Color.White;
             this.result.Name = "result";
+            this.result.Click += new System.EventHandler(this.result_Click);
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Image = global::shef_razreshil_desktop.Properties.Resources._0OtrbkZIyG0;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // shefBoxTrue
+            // 
+            resources.ApplyResources(this.shefBoxTrue, "shefBoxTrue");
+            this.shefBoxTrue.Image = global::shef_razreshil_desktop.Properties.Resources.lzrd_shef_разрешил;
+            this.shefBoxTrue.Name = "shefBoxTrue";
+            this.shefBoxTrue.TabStop = false;
+            // 
+            // shefBoxFalse
+            // 
+            resources.ApplyResources(this.shefBoxFalse, "shefBoxFalse");
+            this.shefBoxFalse.Image = global::shef_razreshil_desktop.Properties.Resources.lzrd_shef_запретил;
+            this.shefBoxFalse.Name = "shefBoxFalse";
+            this.shefBoxFalse.TabStop = false;
             // 
             // shef
             // 
             resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(53)))), ((int)(((byte)(53)))), ((int)(((byte)(53)))));
+            this.Controls.Add(this.shefBoxFalse);
+            this.Controls.Add(this.shefBoxTrue);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.result);
             this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.Name = "shef";
             this.ShowIcon = false;
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shefBoxTrue)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.shefBoxFalse)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -66,5 +102,8 @@
 
         private Button button1;
         private Label result;
+        private PictureBox pictureBox1;
+        private PictureBox shefBoxTrue;
+        private PictureBox shefBoxFalse;
     }
 }
--- shef_razreshil_desktop/Form1.cs
+++ shef_razreshil_desktop/Form1.cs
@@ -1,4 +1,6 @@
+using Microsoft.VisualBasic.ApplicationServices;
 using System.Security.Cryptography.X509Certificates;
+using System.Security.Policy;
 
 namespace shef_razreshil_desktop
 {
@@ -16,12 +18,14 @@ namespace shef_razreshil_desktop
 
         private void button1_Click(object sender, EventArgs e)
         {
-            string shef_res = null;
+            shefBoxFalse.Visible = false;
+            shefBoxTrue.Visible = false;
+            string shef_res;
             string shef = "Шеф";
             string happy_shef = null;
             string evil_shef = null;
 
-            Random xuy = new Random();
+            Random xuy = new();
             int zalupa = xuy.Next(1, 3);
             if (zalupa == 1)
             {
@@ -32,7 +36,7 @@ namespace shef_razreshil_desktop
                 happy_shef = "в честь праздника разрешил покурить в туалетах";
             }
 
-            Random penis = new Random();
+            Random penis = new();
             int davalka = penis.Next(1, 3);
             if (davalka == 1) 
             {
@@ -45,9 +49,9 @@ namespace shef_razreshil_desktop
 
 
             Random rnd = new Random();
-            int x = rnd.Next(0, 10);
+            int rukoblud = rnd.Next(0, 10);
 
-            if (x >= 9)
+            if (rukoblud >= 9)
             {
                 shef_res = shef + " " + happy_shef;
                 result.Text = shef_res;
@@ -58,8 +62,22 @@ namespace shef_razreshil_desktop
                 result.Text = shef_res;
             }
 
+            if (rukoblud >= 9)
+            {
+                shefBoxTrue.Visible = true;
+            }
+            else
+            {
+                shefBoxFalse.Visible = true;
+            }
+
             result.Visible = true;
             
+        }
+
+        private void result_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
